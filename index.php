56b185ed84aa47bf2d58b50fedd36a34c141919f
Tennebris <craftgames.alysson@gmail.com>
2019-08-25 01:58:58 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>DarkArmy</title>
+		<!-- Aquivos de estilo -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/theme.css">
+
+		<!-- Biblioteca em js -->
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+
+		<script type="text/javascript">
+			$(document).ready(function(){
+				// esconder a div login
+				$('.login_index').hide();
+
+				//Quando precionar "Enter" aparece a div login
+				$(document).keypress(function(key){
+					if (key.which == 13){
+						$('.login_index')
+							.hide()
+							.delay('1000')
+							.fadeIn('slow');
+					}
+				});
+				//Quando precionar "Enter" esconde a div login
+				$(document).keypress(function(key){
+					if (key.which == 0){
+						$('.login_index')
+							.delay('1000')
+							.fadeOut('slow');
+					}
+				});
+
+				// redireconar ao clicar no botão "in"
+				$(".in").click(function(){
+					$(window.document.location).attr('href','sign.php');
+				});
+
+				// redireconar ao clicar no botão "out"
+				$(".out").click(function(){
+					$(window.document.location).attr('href','login.php');
+				});
+			});
+		</script>
+	</head>
+	<body class="body_index">
+		<div class="login_index">
+			<div id="sublog">
+				<h1 id="teste"></h1>
+				<img src="img/eliot.gif" class="gifter_eliot">
+				<img src="img/father.gif" class="gifter_father">
+				<div class="bnts">
+					<button class="bnt in"> Cadastrar </button>
+					<button class="bnt out"> Entrar </button>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login | DarkArmy</title>
+	<!-- Aquivos de estilo -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/theme.css">
+
+	<!-- Biblioteca em js -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="js/sweetalert.min.js"></script>
+	<?php
+	session_start();
+	if (isset($_SESSION)){
+		$message = $_SESSION['msg'];
+		$type = $_SESSION['type'];
+		if($type == 'error'){
+			echo '<script type="text/javascript">';
+			echo '$(document).ready(function() {';
+			echo "swal({title:'Erro',text:'$message',icon:'error',button:'Tente novamente'})";
+			echo "});";
+			echo '</script>';
+		}
+		if ($type == 'success') {
+			echo '<script type="text/javascript">';
+			echo '$(document).ready(function() {';
+			echo "swal({title:'Pronto',text:'$message',icon:'success',buttons:false,timer:2000})";
+			echo "});";
+			echo '</script>';
+		}
+		session_unset();
+	}
+	?>
+</head>
+<body class="body_sign">
+	<div class="login_sign">
+		<img src="img/mr.gif" class="banner"><br>
+		<img src="img/evil.png" class="logo"><br>
+		<input type="text" name="user" placeholder="Nick" class="inputs"><br>
+		<input type="password" name="pass" placeholder="Password" class="inputs">
+		<input type="submit" class="bnt_log bnt" value="Entrar">
+	</div>
+</body>
+</html>
