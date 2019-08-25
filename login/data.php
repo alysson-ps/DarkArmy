de62e31315f1101293b744fc6e49f880a8fda919
Tennebris <craftgames.alysson@gmail.com>
2019-08-25 16:09:20 -0300

    correção de bugs

--- login/data.php
+++ login/data.php
@@ -13,7 +13,7 @@ session_start();
 if (!empty($_POST) AND (empty($_POST['user']) OR empty($_POST['pass']))){ 
 	$_SESSION['msg'] = "Todos os campos são obrigatorios";
 	$_SESSION['type'] = "error";
-	header("Location: index.php");
+	header("Location: ../login");
 	exit;
 }
 
@@ -21,23 +21,25 @@ $select = $con->query("SELECT name FROM users WHERE name = '$user'");
 $result = $select->fetch(PDO::FETCH_ASSOC);
 $nick_db = $result['name'];
 
-$select2 = $con->query("SELECT name FROM users WHERE password = '$pass'");
+$select2 = $con->query("SELECT password FROM users WHERE name = '$user'");
 $result2 = $select2->fetch(PDO::FETCH_ASSOC);
 $pass_db = $result2['password'];
+echo $pass_db;
 
 if ($user == $nick_db){
-	if ($pass == $pass_db){
+	if (md5($pass) == $pass_db){
 		header("Location: ../restrito");
 	}else{
 		$_SESSION['msg'] = "A senha está incorreta";
 		$_SESSION['type'] = "error";
-		header("Location: index.php");
+		header("Location: ../login");
 		exit;	
 	}
 }else{
 	$_SESSION['msg'] = "O nick $user não encontrado";
 	$_SESSION['type'] = "error";
-	header("Location: index.php");
+	header("Location: ../login");
 	exit;
 }
+
 ?>
